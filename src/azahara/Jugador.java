b33213b21c7d65125224e889afc3deff3ed6dd89
Azahara.Fonseca <azahara9013@hotmail.com>
2023-02-15 17:58:02 +0100

    javadoc clases

--- src/azahara/Jugador.java
+++ src/azahara/Jugador.java
@@ -1,26 +1,50 @@
 package azahara;
 
+/**
+ * El objetivo de esta clase es proporcionar información sobre un jugador.
+ * 
+ * Para ello, consta de diferentes métodos que nos proporcionan dicha información.
+ * 
+ * @author Azahara
+ */
 public class Jugador {
+
 	private int dorsal;
-    private int numeroTarjetasAmarillas;
-    private int numeroTarjetasRojas;
-  
-    public void ponerDorsal(int dorsal) {
-                  if(dorsal >= 1 && dorsal <= 30) {
-                     this.dorsal = dorsal;
-                  }else {
-                     this.dorsal = -1;
-                 }
-    }
-    
-       public boolean estaExpulsado() {
-                  boolean expulsado = false;                                                  
-                  if(numeroTarjetasAmarillas == 2) {
-                     expulsado = true;
-                  }                          
-                  if(numeroTarjetasRojas == 1) {
-                     expulsado = true;
-                  }                          
+	private int numeroTarjetasAmarillas;
+	private int numeroTarjetasRojas;
+
+	/**
+	 * Método que establece el valor del atributo dorsal. El número pasado por
+	 * parámetro tiene que ser uno elegido entre 1 y 30. Si introducimos un valor
+	 * distinto, el método nos devolverá el número (-1).
+	 * 
+	 * @param dorsal (Número de dorsal que vamos a introducir).
+	 */
+	public void ponerDorsal(int dorsal) {
+		if (dorsal >= 1 && dorsal <= 30) {
+			this.dorsal = dorsal;
+		} else {
+			this.dorsal = -1;
+		}
+	}
+
+	/**
+	 * Método que establece el valor de los atributos número de tarjetas amarillas o rojas.
+	 * Si el número de tarjetas amarillas es igual a 2, el método devuelve true 
+	 * y por tanto el jugador será expulsado.
+	 * Si el número de tarjetas rojas es igual a 1, el método devuelve true y por tanto
+	 * el jugador será expulsado.
+	 * 
+	 * @return expulsado.
+	 */
+	public boolean estaExpulsado() {
+		boolean expulsado = false;
+		if (numeroTarjetasAmarillas == 2) {
+			expulsado = true;
+		}
+		if (numeroTarjetasRojas == 1) {
+			expulsado = true;
+		}  
                   return expulsado;
     }            
 
--- src/azahara/Soldado.java
+++ src/azahara/Soldado.java
@@ -1,23 +1,41 @@
 package azahara;
 
+/**
+ * El objetivo de esta clase es proporcionar información sobre un soldado.
+ * 
+ * Para ello, consta de diferentes métodos que nos proporcionan dicha información.
+ * 
+ * @author Azahara
+ */
 public class Soldado {
-	
-	  private boolean estaMuerto;
-      private int numeroBalas;  
 
-        
-      public boolean puedeDisparar() {
-             if(this.numeroBalas > 0) {
-            	 return true;
-                }                          
-                 return false;
+	private boolean estaMuerto;
+	private int numeroBalas;
 
-      }
+	/**
+	 * Método que establece el valor del atributo número de balas. Si el número pasado
+	 * por parámetro es mayor de cero, el método devuelve true y por tanto, el soldado
+	 * puede disparar. Si por el contrario, el valor introducido es menor o igual a cero,
+	 * el método devuelve false y por tanto el soldado no puede disparar.
+	 * 
+	 * @return true/false
+	 */
+	public boolean puedeDisparar() {
+		if (this.numeroBalas > 0) {
+			return true;
+		}
+		return false;
 
-           public void disparar(Soldado sol) {
-                    this.numeroBalas--;
-                    sol.estaMuerto = true;
-      }
+	}
+
+	/**
+	 * 
+	 * @param sol  Hace referencia al soldado.
+	 */
+	public void disparar(Soldado sol) {
+		this.numeroBalas--;
+		sol.estaMuerto = true;
+	}
 
 }
 
