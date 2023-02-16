ea8821a87a1d09dab1580e1d59107129625fb577
alejandro fernandez prieto <alejandro.mfp@live.com>
2023-02-16 19:37:33 +0100

    creadas las clases Soldado y Jugador, también creados los ficheros para Junit

---
+++ Alejandro/src/modelo/javabeans/Jugador.java
@@ -0,0 +1,118 @@
+package modelo.javabeans;
+
+
+	/**
+	 * 
+	 * El objetivo de esta clase es representar a un jugador que tendrá 3 parámetros.
+	 * Uno para saber su dorsal, otro para saber la cantidad de tarjetas amarillas 
+	 * que ha recibido, y otro para saber la cantidad de tarjetas rojas que ha recibido.
+	 * 
+	 * Tendrá 2 métodos, el primero para asignarle un dorsal, y el otro para saber si debe ser expulsado o no.
+	 * 
+	 * 
+	 * @author Alejandro Martín Fernández Prieto
+	 * @version 1.0
+	 *
+	 */
+	public class Jugador {
+	
+			/**
+			 * Indica el dorsal que tiene el jugador.
+			 */
+		    private int dorsal;
+		    
+		    /**
+		     * Indica la cantidad de tarjetas amarillas que ha recibido.
+		     */
+		    private int numeroTarjetasAmarillas;
+		   
+		    /**
+		     * Indica la cantidad de tarjetas rojas que ha recibido.
+		     */
+		    private int numeroTarjetasRojas;
+	
+		    
+		    /**
+		     * Constructor por defecto
+		     */
+			public Jugador() {
+				super();
+			}
+
+			/**
+			 * Constructor con todos los parámetros de la clase			
+			 * @param dorsal indica el dorsal que tiene el jugador.
+			 * @param numeroTarjetasAmarillas indica la cantidad de tarjetas amarillas que ha recibido.
+			 * @param numeroTarjetasRojas indica la cantidad de tarjetas rojas que ha recibido.
+			 */
+			public Jugador(int dorsal, int numeroTarjetasAmarillas, int numeroTarjetasRojas) {
+				super();
+				this.dorsal = dorsal;
+				this.numeroTarjetasAmarillas = numeroTarjetasAmarillas;
+				this.numeroTarjetasRojas = numeroTarjetasRojas;
+			}
+
+
+			/**
+			 * Este método se encarga de asignar un dorsal al jugador sobre el que apliquemos el método
+			 * 
+			 * Primero revisará si el dorsal introducido se encuentra entre el 1 y el 30 (inclusive), de ser así asignará dicho dorsal al jugador
+			 * En caso contrario asignará el dorsal -1 al jugador
+			 * 
+			 * 
+			 * @param dorsal número que queremos asignar como dorsal al jugador
+			 */
+			public void ponerDorsal(int dorsal) {
+	            if(dorsal >= 1 && dorsal <= 30) {
+	            	this.dorsal = dorsal;
+	            }else {
+	                this.dorsal = -1;
+	            }
+		    }
+	
+			/**
+			 * Este método se encarga de decirnos si un jugador debe ser explusado o no en función de las tarjetas que le hayan mostrado
+			 * Si tiene 2 tarjetas amarilla será expulsado y el método devolverá true
+			 * Si tiene 1 trajeta roja será expulsado y el método devolverá true
+			 * Si no tiene 2 tarjetas amarillas ni 1 tarjeta roja, el jugador no deve ser expulsado y el método devolverá false
+			 * 
+			 * @return si el jugador debe estar expulsado devuelve true, en caso contrario devuelve false
+			 */
+			public boolean estaExpulsado() {
+				boolean expulsado = false;                                                  
+			    if(numeroTarjetasAmarillas == 2) {
+			        expulsado = true;
+			    }                          
+			    if(numeroTarjetasRojas == 1) {
+			    	expulsado = true;
+			    }                          
+			    	return expulsado;
+			}
+			
+			
+			//Getter and Setter
+			public int getDorsal() {
+				return dorsal;
+			}
+
+			public void setDorsal(int dorsal) {
+				this.dorsal = dorsal;
+			}
+
+			public int getNumeroTarjetasAmarillas() {
+				return numeroTarjetasAmarillas;
+			}
+
+			public void setNumeroTarjetasAmarillas(int numeroTarjetasAmarillas) {
+				this.numeroTarjetasAmarillas = numeroTarjetasAmarillas;
+			}
+
+			public int getNumeroTarjetasRojas() {
+				return numeroTarjetasRojas;
+			}
+
+			public void setNumeroTarjetasRojas(int numeroTarjetasRojas) {
+				this.numeroTarjetasRojas = numeroTarjetasRojas;
+			}            
+			
+	}
---
+++ Alejandro/src/modelo/javabeans/Soldado.java
@@ -0,0 +1,101 @@
+package modelo.javabeans;
+
+	/**
+	 * El objetivo de esta clase representar un soldado con 2 atributos. 
+	 * <br>
+	 * El primero estaMuerto para saber si el soldado ha muerto o no,
+	 * y el segundo numeroBalas para saber la cantidad de balas que le quedan al soldado
+	 * <br>
+	 * Tendrá 2 métodos, uno para saber si el soldado puede disparar y otro para realizar el acto de disparar
+	 * <br>
+	 * 
+	 * @author Alejandro Martín Fernández Prieto
+	 * @version 1.0
+	 *
+	 */
+
+	public class Soldado {
+	
+			/**
+			 * Indica si un soldado está muerto o no
+			 */
+		    private boolean estaMuerto;
+		    
+		    /**
+		     * Indica el número de balas que le quedan al soldado
+		     */
+		    private int numeroBalas;
+	    
+		    /**
+		     * Constructor por defecto
+		     */
+			public Soldado() {
+				super();
+			}
+			
+			/**
+			 * Constructor con todos los parámetros de la clase
+			 * @param estaMuerto indica si un soldado está muerto o no
+			 * @param numeroBalas indica el número de balas que le quedan al soldado
+			 */
+
+			public Soldado(boolean estaMuerto, int numeroBalas) {
+				super();
+				this.estaMuerto = estaMuerto;
+				this.numeroBalas = numeroBalas;
+			}
+
+
+
+			/**
+			 * Método que revisa la cantidad de balas que le quedan al soldado y en función de las balas que le queden, nos dirá si puede
+			 * disparar o no.
+			 * 
+			 * @return si el número de balas que le quedan al soldado es mayor que 0, devolverá true, de lo contrario, devolverá false
+			 * 
+			 */
+			
+			public boolean puedeDisparar() {
+		        if(this.numeroBalas > 0) {
+		                      return true;
+		        }                          
+		        return false;
+		    }
+		    
+			/**
+			 * Método que hace que el soldado sobre el que aplicamos el método dispare al soldado que pasamos como parámetro
+			 * 
+			 * Primero resta una bala al soldado que dispara y posteriormente cambia el atributo estaMuerto del soldado que pasamos como parámetro
+			 * a true.
+			 * 
+			 * 
+			 * @param sol representa al soldado al que vamos a disparar
+			 * 
+			 */
+			
+		    public void disparar(Soldado sol) {
+	            this.numeroBalas--;
+	            sol.estaMuerto = true;
+		    }
+
+		    //Getter and Setter
+		    
+			public boolean isEstaMuerto() {
+				return estaMuerto;
+			}
+
+			public void setEstaMuerto(boolean estaMuerto) {
+				this.estaMuerto = estaMuerto;
+			}
+
+			public int getNumeroBalas() {
+				return numeroBalas;
+			}
+
+			public void setNumeroBalas(int numeroBalas) {
+				this.numeroBalas = numeroBalas;
+			}
+	    
+		    
+		    
+	}
