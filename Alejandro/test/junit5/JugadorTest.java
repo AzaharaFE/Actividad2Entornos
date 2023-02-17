1d192218346b0afed2b857905b0552fcb2e89c26
alejandro fernandez prieto <alejandro.mfp@live.com>
2023-02-17 19:37:41 +0100

    añadida documentación, test y soluciones

--- Alejandro/test/junit5/JugadorTest.java
+++ Alejandro/test/junit5/JugadorTest.java
@@ -4,11 +4,93 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import modelo.javabeans.Jugador;
+
 class JugadorTest {
 
+	/**
+	 * @author Alejandro Martín Fernández Prieto
+	 */
+	
+	/**
+	 * En este test vamos a probar el método ponerDorsal() que se encuentra implementado en la clase Jugador.
+	 * <br><br>
+	 * Debemos probar si pasándole como parámetro un número del 1 al 30, este método asigna
+	 * al jugador sobre el que se aplica el dorsal pasado por parámetro.
+	 * También debemos probar si pasándole como parámetro un número menor que 1 y mayor que 30, 
+	 * este método asigna el número -1 al jugador sobre el que se aplica.
+	 * <br><br>
+	 * Para ello implementaremos un bucle for que pase como parámetro al método todos los números
+	 * del 0 al 100. Cuando el número sea igual a 0 o mayor que 30, el método debe asignar -1 al
+	 * dorsal del jugador, en caso contrario debe asiganr el número pasado como parámetro en el
+	 * método.
+	 */
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testPonerDorsal() {
+		
+		Jugador jug1 = new Jugador();
+		
+		int resultadoEsperado = 0;
+		int resultadoObtenido = 0;
+		
+		for (int i=0; i<=100; i++) {
+			jug1.ponerDorsal(i);
+			if (i==0 || i>30) {
+				resultadoEsperado=-1;
+				resultadoObtenido=jug1.getDorsal();
+				assertEquals(resultadoEsperado, resultadoObtenido);
+			}else {
+				resultadoEsperado=i;
+				resultadoObtenido=jug1.getDorsal();
+				assertEquals(resultadoEsperado, resultadoObtenido);	
+			}
 
+		}
+	}
+	
+	/**
+	 * En este test vamos a probar el método estaExpulsado() que se encuentra implementado en la clase Jugador.
+	 * <br><br>
+	 * Debemos probar si cuando el jugador tiene menos de 2 tarjetas amarillas y 
+	 * menos de 1 tarjeta roja, el método devuelve false porque el jugador no está expulsado
+	 * También debemos probar si el método devuelve true cuando el jugador tiene 2 o más 
+	 * tarjetas amarilla o 1 o más tarjetas rojas, ya que en este caso el jugador estaría expulsado
+	 * <br><br>
+	 * Para ello implementaremos un bucle for que se repita 12 veces para así poder probar todas
+	 * las posibles combinaciones de tarjetas amarillas y tarjetas rojas que hay desde 0 a 3 tarjetas
+	 * amarillas y desde 0 a 2 tarjetas rojas.
+	 * Si el número de tarjetas amarillas es menor de 2 y el número de tarjetas rojas es menor de 1,
+	 * el jugador no estará expulsado, por lo que el método devolverá false (solo existen dos posibilidades
+	 * de que esto ocurra en el test, que el jugador tenga 0, tarjetas de ambos colores o que solamente tenga
+	 * una tarjeta amarilla), en el resto de casos el método devolverá true, ya que el jugador al menos tendrá
+	 * 2 o más tarjetas amarillas, 1 o más tarjetas rojas o ambos casos ocurrirían a la vez, por lo
+	 * tanto el jugador estará expulsado.
+	 */
+	
+	@Test
+	void testEstaExpulsado() {
+		Jugador jug1 = new Jugador();
+		
+		int a = 0;
+		int r = 0;
+		
+		for (int i=0; i<12; i++) {
+			
+			jug1.setNumeroTarjetasAmarillas(a);
+			jug1.setNumeroTarjetasRojas(r);
+			
+			if (a<2 && r<1)
+			assertFalse(jug1.estaExpulsado());
+			else
+			assertTrue(jug1.estaExpulsado());	
+			
+			if (a==3) {
+				a=0;
+				r++;
+			}else
+				a++;
+		}
+		
+	}
 }
--- Alejandro/test/junit5/SoldadoTest.java
+++ Alejandro/test/junit5/SoldadoTest.java
@@ -4,11 +4,87 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import modelo.javabeans.Soldado;
+
 class SoldadoTest {
 
+	/**
+	 * @author Alejandro Martín Fernández Prieto
+	 */
+	
+	/**
+	 * En este test vamos a probar el método puedeDisparar() que se encuentra implementado en la clase Soldado.
+	 * <br><br>
+	 * Debemos probar si al aplicar este método sobre un soldado que tiene un número positivo
+	 * en su argumento numeroBalas, el método devuelve true, y por el contrario si el argumento numeroBalas
+	 * del soldado sobre el que aplicamos en método es 0, el método devolverá false, ya que el
+	 * soldado no puede disparar.
+	 * <br><br>
+	 * Para ello crearemos dos nuevos objetos de la clase Soldado, al primero le asignaremos un
+	 * número positivo a su argumento numeroBalas, y al segundo le asiganremos 0 al mismo argumento.
+	 * Al aplicar el método sobre el primer objeto nos devolverá true, ya que el soldado puede disparar,
+	 * en el caso del segundo objeto, el método devolverá false, ya que el soldado no puede disparar
+	 * al no tener balas.
+	 */
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPuedeDispararBalas() {
+		Soldado sol1 = new Soldado();
+		Soldado sol2 = new Soldado();
+		
+		sol1.setNumeroBalas(3);
+		sol2.setNumeroBalas(0);
+		
+		assertTrue(sol1.puedeDisparar());
+		assertFalse(sol2.puedeDisparar());
+	}
+	
+	/**
+	 * En este test vamos a probar el método disparar() que se encuentra implementado en la clase Soldado.
+	 * <br><br>
+	 * Debemos probar si despues de aplicar el método sobre un soldado, a este se le resta una bala de su
+	 * argumento numeroBalas en caso de tener un número positivo en dicho argumento antes de aplicar el método sobre él.
+	 * A su vez debemos comprobar que el argumento estaMuerto del soldado que pasamos como parámetro al método pasa a true.
+	 * En caso de que el soldado sobre el que apliquemos el método tenga en su argumento numeroBalas un número menor a 1,
+	 * el argumento estaMuerto del soldado que pasamos como parámetro no cambiará, ya que el soldado sobre 
+	 * el que apliquemos el método no tiene balas para disparar, por lo tanto tampoco se le restará una bala de su
+	 * argumento numeroBalas.
+	 * <br><br>
+	 * Para ello crearemos dos nuevos objetos de la clase Soldado, al primero le asignaremos un
+	 * número positivo a su argumento numeroBalas, y al segundo le asiganremos 0 al mismo argumento.
+	 * Además asignaremos false al argumento estaMuerto de ambos soldados.
+	 * Primero haremos que el segundo soldado dispare al primero. Como el segundo soldado no tiene balas,
+	 * el número de balas del segundo soldado seguirá siendo 0, y como al no tener balas no puede
+	 * disparar, el primer soldado no estará muerto, por lo que su argumento estaMuerto seguirá siendo false.
+	 * A continuación haremos que el primer soldado dispare al segundo, en este caso al argumento numeroBalas
+	 * del primer soldado se le habrá restado una unidad, ya que ha disparado una bala, y el argumento
+	 * estaMuerto del segundo soldado habrá cambiado a true, ya que ahora está muerto porque el primer soldado
+	 * le ha disparado. 
+	 */
+	
+	@Test
+	void testDisparar() {
+		Soldado sol1 = new Soldado(false, 2);
+		Soldado sol2 = new Soldado(false, 0);
+		
+		sol2.disparar(sol1);
+		
+		int resultadoEsperado1 = 0;
+		int resultadoObtenido1 = sol2.getNumeroBalas();
+		
+		assertEquals(resultadoEsperado1, resultadoObtenido1);
+		assertFalse(sol1.isEstaMuerto());
+				
+		sol1.disparar(sol2);
+		
+		int resultadoEsperado2 = 1;
+		int resultadoObtenido2 = sol1.getNumeroBalas();
+		
+		assertEquals(resultadoEsperado2, resultadoObtenido2);		
+		assertTrue(sol2.isEstaMuerto());
+		
+		
+		
 	}
 
 }
